b1829a80b6d59609cdddae10c23273f4dded4605
Beto <josealberto.fb15@gmail.com>
2023-01-19 02:06:06 -0600

    ci: funcion obtener datos

--- calendario.js
+++ calendario.js
@@ -39,5 +39,42 @@ function validaAnioBisiesto(anio){
     }
 }
 
+function faseLunar(dia,mes,anio)
+{
+    let fase = 0;
+    let totalDias = 0;
+    var c = e = 0;
+
+    if (mes < 3) {
+        anio--;
+        mes += 12;
+    }
+
+    ++mes;
+
+    c = 365.25 * anio;
+
+    e = 30.6 * mes;
+
+    totalDias = c + e + dia - 694039.09; 
+
+    totalDias /= 29.5305882;
+
+    fase = parseInt(totalDias); 
+
+    totalDias -= fase; 
+
+    fase = Math.round(totalDias * 8);
+
+    if (fase >= 8 ) {
+        fase = 0; 
+    }
+    let nombreDeFase = ["Luna nueva", "Luna Creciente", "Cuarto creciente", "Luna gibosa creciente", "Luna llena", "Luna gibosa menguante", "Cuarto menguante", "Luna menguante"];
+    
+    return 
+    console.log(nombreDeFase[fase]);
+}
+
+
 
 
